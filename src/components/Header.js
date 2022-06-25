7fb5774f890297e87c7d40921375d9f0ba1f06ad
Redmist-bit <bayuadeirawan79@gmail.com>
2022-06-25 20:47:47 +0700

    done

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,12 @@
 // Kalian bisa menambahkan CSS di src/components/Header.css
 import React from 'react';
-
+import "./Header.css";
 const Header = () => {
     return (
-        <></>
+        <div id="header">
+            <h1>Call a friend</h1>
+            <p>your friendly contact app</p>
+        </div>
     )
 }
 
